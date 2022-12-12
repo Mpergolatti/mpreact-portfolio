b858389aeae5b916b86c83e85cbe3a4471aa9eff
Mpergolatti <Mpergolatti@gmail.com>
2022-12-11 22:25:12 -0500

    routing issues

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react'
-// import { Route, Routes, NavLink, HashRouter } from 'react-router-dom'
-// import About from './components/About'
+import { Route, Routes, NavLink, HashRouter } from 'react-router-dom'
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 
 class App extends Component {
   render() {
     return (
-
+      <HashRouter>
         <div>
           <h1>Mike Pergolatti</h1>
           <ul className='header'>
-            <li><a href='./components/About'>About Me</a></li>
-            <li><a href='./components/Projects'>Projects</a></li>
-            <li><a href='./components/Contact'>Contact Me</a></li>
+            <li><NavLink to='/'>About Me</NavLink></li>
+            <li><NavLink to='/Projects'>My Projects</NavLink></li>
+            <li><NavLink to='/Contact'>Contact Me</NavLink></li>
+
           </ul>
           <div className='content'>
 
+          <Routes>
+            <Route exact path='/' element={<About />} />
+            <Route path='/Projects' element={<Projects />} />
+            <Route path='/Contact' element={<Contact />} />
+          </Routes>
+
           </div>
         </div>
+      </HashRouter>
       
     )
   }
