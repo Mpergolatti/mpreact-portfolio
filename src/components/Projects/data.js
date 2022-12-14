482727cac2f07d84448f7bc7be818db8c469f80a
Mpergolatti <Mpergolatti@gmail.com>
2022-12-14 18:39:25 -0500

    commit current work

---
+++ src/components/Projects/data.js
@@ -0,0 +1,73 @@
+
+const projects = [
+  {
+    id: 0,
+    title: 'title',
+    languages: 'languages',
+    packages: 'packages',
+    images: 'image location',
+    description: 'description',
+    repo: 'repo',
+    live: 'live'
+  },
+  {
+    id: 1,
+    title: 'title',
+    languages: 'languages',
+    packages: 'packages',
+    images: 'image location',
+    description: 'description',
+    repo: 'repo',
+    live: 'live'
+  },
+  {
+    id: 2,
+    title: 'title',
+    languages: 'languages',
+    packages: 'packages',
+    images: 'image location',
+    description: 'description',
+    repo: 'repo',
+    live: 'live'
+  },
+  {
+    id: 3,
+    title: 'title',
+    languages: 'languages',
+    packages: 'packages',
+    images: 'image location',
+    description: 'description',
+    repo: 'repo',
+    live: 'live'
+  },
+  {
+    id: 4,
+    title: 'title',
+    languages: 'languages',
+    packages: 'packages',
+    images: 'image location',
+    description: 'description',
+    repo: 'repo',
+    live: 'live'
+  },
+  {
+    id: 5,
+    title: 'title',
+    languages: 'languages',
+    packages: 'packages',
+    images: 'image location',
+    description: 'description',
+    repo: 'repo',
+    live: 'live'
+  },
+  {
+    id: 6,
+    title: 'title',
+    languages: 'languages',
+    packages: 'packages',
+    images: 'image location',
+    description: 'description',
+    repo: 'repo',
+    live: 'live'
+  },
+]
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,22 +1,87 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Projects extends Component {
-  render() {
+const Projects = ({}) => {
+  const [currentProject, setCurrentProject] = useState()
+
+  const [project] = useState([
+    {
+      id: 0,
+      title: 'title',
+      languages: 'languages',
+      packages: 'packages',
+      images: 'image location',
+      description: 'description',
+      repo: 'repo',
+      live: 'live'
+    },
+    {
+      id: 1,
+      title: 'title',
+      languages: 'languages',
+      packages: 'packages',
+      images: 'image location',
+      description: 'description',
+      repo: 'repo',
+      live: 'live'
+    },
+    {
+      id: 2,
+      title: 'title',
+      languages: 'languages',
+      packages: 'packages',
+      images: 'image location',
+      description: 'description',
+      repo: 'repo',
+      live: 'live'
+    },
+    {
+      id: 3,
+      title: 'title',
+      languages: 'languages',
+      packages: 'packages',
+      images: 'image location',
+      description: 'description',
+      repo: 'repo',
+      live: 'live'
+    },
+    {
+      id: 4,
+      title: 'title',
+      languages: 'languages',
+      packages: 'packages',
+      images: 'image location',
+      description: 'description',
+      repo: 'repo',
+      live: 'live'
+    },
+    {
+      id: 5,
+      title: 'title',
+      languages: 'languages',
+      packages: 'packages',
+      images: 'image location',
+      description: 'description',
+      repo: 'repo',
+      live: 'live'
+    },
+    {
+      id: 6,
+      title: 'title',
+      languages: 'languages',
+      packages: 'packages',
+      images: 'image location',
+      description: 'description',
+      repo: 'repo',
+      live: 'live'
+    },
+  ])
+
     return(
-    <div>
-      <h2>My Projects</h2>
-      <p>This is going to be a description of my Projects</p>
-      <ol>
-        <li>Project 1</li>
-        <li>Project 2</li>
-        <li>Project 3</li>
-        <li>Project 4</li>
-        <li>Project 5</li>
-        <li>Project 6</li>
-      </ol>
-    </div>
+      <section>
+
+      </section>
     )
-  }
+  
 }
 
 export default Projects
