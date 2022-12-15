ce4917cf35860b2bb227531045bbd0e411a4b542
Mpergolatti <Mpergolatti@gmail.com>
2022-12-15 18:51:32 -0500

    image issues

--- src/components/ProjectData/index.js
+++ src/components/ProjectData/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
+
 function ProjectData(props) {
   return(
     <div>
+      <div>
+        {props.project.map((project) => (
+          <div>
+            <div>
+              <div>
+                <figure>
+                  <a href={project.live} target='_blank' rel='noreferrer'>
 
+                    <img src={project.images} alt='Placeholder' />
+
+                  </a>
+                </figure>
+              </div>
+              <div>
+                <p className='' key={project.id}>
+                  {project.title}
+                </p>
+                {project.description}
+                <br />
+                Languages: {project.languages}
+                <br />
+                Packages used: {project.packages}
+                <br />
+                <div>
+                  <a href={project.repo} target='_blank'>Repo</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
