deabbeef6903a73ebd6e7cef7a641fa90c9636e0
Mpergolatti <Mpergolatti@gmail.com>
2022-12-16 21:51:15 -0500

    issue with keeping footer at bottom of screen

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes, NavLink, HashRouter } from 'react-router-dom'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import Clock from 'react-live-clock'
+// import DatePicker from 'react-date-picker'
 
 
 class App extends Component {
@@ -10,7 +12,10 @@ class App extends Component {
     return (
       <HashRouter>
         <div>
-          <h1>Mike Pergolatti</h1>
+          <div className='name_clock'>
+            <h1>Mike Pergolatti</h1>
+            <Clock format={'h:mm:ss a'} ticking={true} timezone={'US/Eastern'} className='clock' />
+          </div>
           <ul className='header'>
             <li><NavLink to='/'>About Me</NavLink></li>
             <li><NavLink to='/Projects'>My Projects</NavLink></li>
@@ -28,8 +33,13 @@ class App extends Component {
           </div>
         </div>
         <footer>
-          <div>
-            <h1>This is the footer</h1>
+          <div className='footer_pin'>
+            <ul className='footer'>
+              <li>GitHub</li>
+              <li>Facebook</li>
+              <li>Linked In</li>
+              {/* <DatePicker /> */}
+            </ul>
           </div>
         </footer>
       </HashRouter>
